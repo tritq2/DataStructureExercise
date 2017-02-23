7f7f1e80ec8caf0ff9c7d53aff2a50cca630109e
tritq2 <tritq2@zmail.vn>
2017-02-23 10:47:16 +0700

    array c1 submit2

--- Arr_c1.js
+++ Arr_c1.js
@@ -10,5 +10,29 @@ function main() {
         }
     }
     process.stdout.write(msg);
+}
 
+function main() {
+    var n = parseInt(readLine());
+    arr = readLine().split(' ');
+    //arr = arr.map(Number);
+
+    if(n <1){
+        process.stdout.write('');
+    }
+    let output = '';
+    let loop = Math.ceil(n/2) -1;
+    let isOdd = (n%2)==1;
+    if(isOdd){
+        output += arr[loop]
+    }
+    else{
+        output += arr[n- loop -1] + ' ' + arr[loop]
+    }
+    for(let i =loop-1 ;i>=0 ; i--){
+
+        output = arr[n-i-1] +' '+ output+ ' '+ arr[i];
+
+    }
+    process.stdout.write(output);
 }
