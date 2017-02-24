25c659937864cd00b3837b616a49bc21ab33fc5f
tritq2 <tritq2@zmail.vn>
2017-02-24 13:59:56 +0700

    fix

--- Arr_c1.js
+++ Arr_c1.js
@@ -1,38 +1,20 @@
+https://www.hackerrank.com/challenges/arrays-ds
+/*
+Given an array, , of  integers, print each element in reverse order as a single line of space-separated integers.
 
-function main() {
-    var n = parseInt(readLine());
-    arr = readLine().split(' ');
-    let msg = '';
-    for(let i =n-1 ; i>=0; i--){
-        msg += arr[i];
-        if(i!=0){
-            msg+= ' ';
-        }
-    }
-    process.stdout.write(msg);
-}
 
+*/
 function main() {
     var n = parseInt(readLine());
     arr = readLine().split(' ');
     //arr = arr.map(Number);
-
-    if(n <1){
-        process.stdout.write('');
+    let loop = Math.ceil(n/2);
+    for(let i =0; i< loop; i++){
+        let tmp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = tmp
     }
-    let output = '';
-    let loop = Math.ceil(n/2) -1;
-    let isOdd = (n%2)==1;
-    if(isOdd){
-        output += arr[loop]
-    }
-    else{
-        output += arr[n- loop -1] + ' ' + arr[loop]
-    }
-    for(let i =loop-1 ;i>=0 ; i--){
-
-        output = arr[n-i-1] +' '+ output+ ' '+ arr[i];
-
-    }
+    let output = arr.join(' ');
     process.stdout.write(output);
+
 }
